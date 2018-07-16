a77bf8b2588290d62d9d0970ee6094bc060a5b32
cody budge <codybudge@gmail.com>
2018-07-16 08:49:40 -0600

    updating store to be more manageable

--- wwwsrc/src/components/Vault.vue
+++ wwwsrc/src/components/Vault.vue
@@ -1,30 +1,26 @@
 <template>
-  <div class="vaults">
-    <div class="row">
-      <div class="col">
-        <h1>Vault card here</h1>
-        <h3>Keeps here</h3>
+    <div class="vaults">
+      <div class="row">
+        <div class="col">
+          <h1>Vault card here</h1>
+          <h3>Keeps here</h3>
+        </div>
       </div>
+  
     </div>
-
-  </div>
-</template>
-
-<script>
-  export default {
-    name: '',
-    data() {
-      return {
-
-      }
-    },
-    computed: {},
-    methods: {}
-  }
-
-</script>
-
-<style>
-
-
-</style>
+  </template>
+  
+  <script>
+    export default {
+      name: '',
+      data() {
+        return {
+        }
+      },
+      computed: {},
+      methods: {}
+    }
+  </script>
+  
+  <style>
+  </style>
