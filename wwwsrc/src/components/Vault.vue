e5e9bafd5b5f1872091926b5261d032332dd490f
cody budge <codybudge@gmail.com>
2018-07-16 21:09:17 -0600

    finished vault.vue

--- wwwsrc/src/components/Vault.vue
+++ wwwsrc/src/components/Vault.vue
@@ -7,7 +7,9 @@
             <button type="submit">Submit</button>
          </form>
          <div v-for="vault in vaults">
-
+          <router-link :to="{name: 'ViewVault'}">
+              <button @click="setVault(vault)">{{vault.description}}</button>
+          </router-link>
          </div>
         </div>
       </div>
@@ -17,13 +19,27 @@
   
   <script>
     export default {
-      name: '',
+      name: 'Vault',
       data() {
         return {
         }
       },
-      computed: {},
-      methods: {}
+      computed: {
+        vault(){
+          return this.$store.state.vaults
+        },
+        currentUser(){
+          return this.$store.state.currentUser
+        }
+      },
+      methods: {
+        addNewVault(){
+          this.$store.dispatch('addNewVault', this.newVault)
+        },
+        setVault(){
+          this.$store.dispatch('setVault', vault)
+        }
+      }
     }
   </script>
   
